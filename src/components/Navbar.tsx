2d51243e400ab759f25c0ea06a4730deaab7800b
Divakar Navik <divakarnavik03@gmail.com>
2026-04-05 20:46:54 +0530

    height fixed of logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar: React.FC<NavbarProps> = ({ activeSection, onNavigate }) => {
           <div className="flex items-center gap-2 cursor-pointer" onClick={() => onNavigate('hero')}>
             <div className="relative">
               <div className="flex items-center justify-center">
-                <img src={logo} alt="Logo" className="h-10 w-auto object-contain" />
+                <img src={logo} alt="Logo" className="h-15 w-auto object-contain" />
               </div>
             </div>
             <span className="text-xl font-bold bg-gradient-to-r from-amber-600 to-orange-600 bg-clip-text text-transparent">
